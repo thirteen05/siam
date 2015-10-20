d98e2b0c45523badfdb228b59d5d0c80e3441a37
Justin R <justin@thirteen05.com>
2015-10-20 18:15:44 -0400

    Initial Homepage Structure

--- sections/home/section-home-example.php
+++ sections/home/section-home-example.php
@@ -31,12 +31,56 @@
         </div>
       <div class="col-md-10 text-center">
         <div class="main-section">
-            <div class="homeframe1">
-            
-            </div>  
+            <div class="homeframe1"></div> 
+            <div class="quote">
+                At the heart of Siam Seas is a passion for reintroducing the time-honored ingredients and philosophies of Southeast Asia to a new generation across the globe. 
+            </div>
+            <div class="homeframe2"></div>
+            <div class="quote">
+                We carefully select the most effective natural and organic Southeast Asian herbs for our formulas
+            </div>
+            <div class="row home-cta">
+                <div class="col-md-4">
+                    <div class="cta">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/home-face.png" />
+                        <h2>Face</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="cta">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/home-body.png" />
+                        <h2>Body</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="cta">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/home-hair.png" />
+                        <h2>Hair</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="homeframe3"></div>
+            <div class="quote">
+                “The soap scents are fantastic. I really like how it moisturizes my skin especially during the winter season. It makes my skin feel great!” – Shane, Manhattan, NY
+            </div>
+            <div class="homeframe4"></div>
+            <div class="quote">
+                We make each and every one of our products by hand in Thailand, ensuring that every step is performed by real people and not machines
+            </div>
+            <div class="homeframe5"></div>
+            <div class="quote">
+                We believe that true healing starts from the inside, and all of our products are formulated to encourage holistic harcmony and well-being
+            </div>
+            <div class="homeframe6"></div>
+            <footer>
+                <a href="#">Custom & Favors </a> / <a href="#">Shipping</a> / <a href="#">Exchanges</a> / <a href="#">Returns</a>       <a href="#">Testimonials</a>       <a href="#">News</a> / <a href="#">Special Offers</a>
+            </footer>
         </div>
       </div>
     </div>
   </div>
     <a href="#">This is the link</a>
-</section>4BxZW+)=X;
+</section>
