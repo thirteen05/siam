c4d1a25b357f1b00a2e406462491eab8c2e5f228
Justin R <justin@thirteen05.com>
2015-10-20 13:19:53 -0400

    Added Sidebar

--- sections/home/section-home-example.php
+++ sections/home/section-home-example.php
@@ -1,11 +1,42 @@
 <section id="section-home-example">
-  <div class="container">
+  <div class="container-full">
     <div class="row">
-      <div class="col-md-12 text-center">
-        <img id="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo-header.svg" width="300" />
+        <div class="col-md-2 text-center">
+            <div class="sidebar">
+                <img id="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo-header.svg" width="300" />
+                <?php wp_nav_menu(); ?>
+                <ul>
+                    <li>
+                        <a href="#">Our Story</a>
+                    </li>
+                    <li>
+                        <a href="#">What's in it?</a>
+                    </li>
+                    <li>
+                        <a href="#">Art of Giving</a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="#">Press</a>
+                    </li>
+                    <li>
+                        <a href="#">Wholesale</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      <div class="col-md-10 text-center">
+        <div class="main-section">
+            <div class="homeframe1">
+            
+            </div>  
+        </div>
       </div>
     </div>
   </div>
-    
     <a href="#">This is the link</a>
-</section>
+</section>4BxZW+)=X;
